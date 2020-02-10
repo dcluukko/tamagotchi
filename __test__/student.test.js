eeaab7ac8826054c9a7586d2f776a7cead961391
Dana Luukko and Keturah Howard <keturah@>
2020-02-10 11:42:43 -0800

    created test for testing decrimenting time

--- __test__/student.test.js
+++ __test__/student.test.js
@@ -1,19 +1,27 @@
 import { Student } from '../src/student.js';
+import { exportAllDeclaration } from '@babel/types';
 
 describe('Student', () => {
-    test('should create new Code-agatchi with name', () => {
-        let input = 'Carol'
-        let student = new Student(input);
-        expect(student.name).toEqual('Carol');
-    }); 
-    let input;
-    let student;
-    beforeEach(() => {
-        input = 'Carol';
-        student = new Student(input);
-    })
-    test('should set energy to 10', () => {
-       student.coffeeBreak();
-        expect(student.energy).toEqual(10);
-    })
+  let input;
+  let student;
+  jest.useFakeTimers();
+
+  test('should create new Code-agatchi with name', () => {
+    let input = 'Carol'
+    let student = new Student(input);
+    expect(student.name).toEqual('Carol');
+  }); 
+  beforeEach(() => {
+    input = 'Carol';
+    student = new Student(input);
+  })
+  test('should set energy to 10', () => {
+    student.coffeeBreak();
+    expect(student.energyLevel).toEqual(10);
+  })
+  test('should decrease energy over time',() => {
+    student.setEnergy();
+    jest.advanceTimersByTime(3001);
+    expect(student.energyLevel).toEqual(10);
+  })
 });
