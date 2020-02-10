8a15902920ebf2e92b38b49955e5684e74921102
Dana Luukko and Keturah Howard <dana@>
2020-02-10 14:05:52 -0800

    clean up code comments

--- src/student.js
+++ src/student.js
@@ -5,63 +5,28 @@ export class Student {
     this.networkLevel = 10;
     this.name = name; 
 }
-//Energy Level
+
+life() {
+  let clock = setInterval(() => {
+    this.energyLevel-- ;
+    this.studyLevel--;
+    this.networkLevel--;
+    if (this.studyLevel === 0 || this.energyLevel === 0 || this.networkLevel === 0) {
+      clearInterval(clock);
+      console.log('isliving');
+    }
+  }, 1000);
+}
+
+
   coffeeBreak() {
     this.energyLevel = 10
   }
-  
-  life() {
-    let clock = setInterval(() => {
-     this.energyLevel-- ;
-     this.studyLevel--;
-     this.networkLevel--;
-     if (this.studyLevel === 0 || this.energyLevel === 0 || this.networkLevel === 0) {
-       clearInterval(clock);
-       console.log('isliving');
-     }
-    }, 1000);
-  }
-
-
-  // setEnergy() {
-  //   let energyClock = setInterval(() => {
-  //       this.energyLevel--;
-  //   }, 1000);
-  // }
-
-// Study Level
-  studyTime() {
+  study() {
     this.studyLevel = 10
   }
-
-  // setStudy() {
-  //   let energyClock = setInterval(() => {
-  //     this.energyLevel--;
-  // }, 1000);
-  // }
-
-//Network Level
-  meetUpTime() {
+  meetUp() {
     this.networkLevel = 10
   }
-
-  // setNetwork() {
-  //   setInterval(() => {
-  //     this.networkLevel--;
-  //   }, 1000);
-  // }
-
-  // kickedOut(interval) {
-  //   clearInterval(interval);
-  //   console.log(interval)
-  //   console.log('interval')
-  // }
-
-  // isLiving() {
-  //   if (this.studyLevel === 0 || this.energyLevel === 0 || this.networkLevel === 0) {
-  //     kickedOut(setEnergy());
-  //     console.log('isliving')
-  //   }
-  // }
 }
 
