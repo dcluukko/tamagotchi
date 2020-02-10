aa8999091682dfa9bfbd21ea4e1372bdf8aeed78
Dana Luukko and Keturah Howard <dana@>
2020-02-10 11:19:06 -0800

    create new test for energy level

--- __test__/student.test.js
+++ __test__/student.test.js
@@ -4,7 +4,16 @@ describe('Student', () => {
     test('should create new Code-agatchi with name', () => {
         let input = 'Carol'
         let student = new Student(input);
-        
         expect(student.name).toEqual('Carol');
     }); 
+    let input;
+    let student;
+    beforeEach(() => {
+        input = 'Carol';
+        student = new Student(input);
+    })
+    test('should set energy to 10', () => {
+       // student.coffeeBreak();
+        expect(student.energy).toEqual(10);
+    })
 });
