f81704ddfab3ccef7c843950b4dd0899055eb1b9
Dana Luukko and Keturah Howard <keturah@>
2020-02-11 16:52:35 -0800

    slight change to function format

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import './style.css';
 
 
 $(document).ready(function () {
+  let level = $('#level');
   function displayToGameScreen(student) {
     $('#studentName').html(student.name);
-    $('#level').html(student.level);
+    level.html(student.level);
   }
   function meterBar(student) {
-    // let drain = setInterval(() => {
-    setInterval(() => {
+    let drain = setInterval(() => {
+    // setInterval(() => {
       // $("#coffeeBar").text(student.energyLevel + "0%");
       console.log(student.energyLevel + "0%");
       // $("#studyBar").text(student.studyLevel + "0%");
@@ -20,23 +21,51 @@ $(document).ready(function () {
       $('#coffeeBar').css("width", student.energyLevel + "0%");
       $('#studyBar').css("width", student.studyLevel + "0%");
       $('#networkBar').css("width", student.networkLevel + "0%");
-      if(student.energyLevel <= student.max/2 ) {
+      level.html(student.level);
+      if (student.networkLevel <= 0 || student.studyLevel <= 0 || student.energyLevel <= 0) {
+        $('#happy').hide();
+        $('#lonely').hide();
+        $('#dumb').hide();
+        $('#tired').hide();
+        $('#grad').hide();
+        $('#dead').show();
+      } else if (student.energyLevel <= student.max / 2) {
         $('#happy').hide();
         $('#lonely').hide();
         $('#dumb').hide();
         $('#tired').show();
-      } else if (student.studyLevel <= student.max/2) {
+        $('#grad').hide();
+        $('#dead').hide();
+      } else if (student.studyLevel <= student.max / 2) {
         $('#happy').hide();
         $('#lonely').hide();
         $('#dumb').show();
         $('#tired').hide();
-      } else if (student.networkLevel <= student.max/2) {
+        $('#grad').hide();
+        $('#dead').hide();
+      } else if (student.networkLevel <= student.max / 2) {
         $('#happy').hide();
         $('#lonely').show();
         $('#dumb').hide();
         $('#tired').hide();
-      }
-      // clearInterval(drain);
+        $('#grad').hide();
+        $('#dead').hide();
+      } else if (student.level >= 7) {
+        $('#happy').hide();
+        $('#lonely').hide();
+        $('#dumb').hide();
+        $('#tired').hide();
+        $('#grad').show();
+        $('#dead').hide(); 
+        clearInterval(drain);
+      } else if (student.energyLevel > student.max / 2 && student.studyLevel > student.max / 2 && student.studyLevel > student.max / 2) {
+        $('#happy').show();
+        $('#lonely').hide();
+        $('#dumb').hide();
+        $('#tired').hide();
+        $('#grad').hide();
+        $('#dead').hide(); 
+      }  
     }, 1000);
   }
   let student;
